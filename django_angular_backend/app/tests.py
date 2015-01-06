c2b8ec3cd64d764a936f06462e47f65dd7712c24
shurik <stu.shuriK@gmail.com>
2015-01-06 10:50:01 +0200

    clean test

--- django_angular_backend/app/tests.py
+++ django_angular_backend/app/tests.py
@@ -10,15 +10,6 @@ from django.test import TestCase
 from django.test.client import Client
 
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-
 class HttpTest(TestCase):
     def test_home(self):
         c = Client()
